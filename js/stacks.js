40c90e60895d554118eb7f41b329945097ad3792
Pablo Osinaga <paguos@gmail.com>
2017-07-09 15:28:46 +0200

    Outsource code to tools.js

--- js/stacks.js
+++ js/stacks.js
@@ -232,44 +232,4 @@ function chooseCardIndex(oldIndex) {
 	return index;
 }
 
-// ### UTILITIES ###
-
-function copyArray(arry) {
-	var newArry = [];
-	for (var i = 0; i < arry.length; i++) {
-		newArry.push(arry[i]);
-	}
-	return newArry;
-}
-
-function removeChildNodes(myNode) {
-	while (myNode.firstChild) {
-		myNode.removeChild(myNode.firstChild);
-	}
-
-}
-
-function setCookie(cname, cvalue, exdays) {
-	var d = new Date();
-	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-	var expires = "expires=" + d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-	var name = cname + "=";
-	var decodedCookie = decodeURIComponent(document.cookie);
-	var ca = decodedCookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return "";
-}
-
 init();
